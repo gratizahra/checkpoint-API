efa5902251f13372acf9a74a9a461fc516a97ea9
gratizahra <gratizahra@gmail.com>
2021-06-14 20:42:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserList from './Component/UserList';
+
+function App() {
+ 
+ return (
+   <>
+   <h1>User List</h1>
+   <UserList/>
+   </>
+ );
+}
+export default App;
